d67a6f7f9ae7c4972b98c3d4b4574477c2cfef0f
Southrop <southrop113@gmail.com>
2017-04-20 02:24:08 +0900

    Finish implementing basic oauth flow

--- app/src/main/java/com/kirakiratter/android/LoginActivity.java
+++ app/src/main/java/com/kirakiratter/android/LoginActivity.java
@@ -1,21 +1,23 @@
 package com.kirakiratter.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 
 import com.kirakiratter.android.util.OkHttpClientHelper;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnTextChanged;
 import jastodon.MastodonApiClient;
-import jastodon.entities.OAuthTokens;
+import jastodon.models.oauth.AccessToken;
+import jastodon.models.oauth.ClientTokens;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,13 +31,16 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getSimpleName();
     private static final String APP_SCOPE = "read write follow";
 
-    @BindView(R.id.edittext_domain) EditText domainEditText;
+    @BindView(R.id.edittext_domain)
+    TextInputEditText domainEditText;
 
+    private MastodonApiClient mastodonApi;
     private SharedPreferences preferences;
 
     private String domain = null;
     private String clientId = null;
     private String clientSecret = null;
+    private String redirectUrl = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +58,31 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mastodonApi = new MastodonApiClient(OkHttpClientHelper.getOkHttpClient(), domain);
+
+        redirectUrl = getString(R.string.redirect_uri_scheme) + "://" + getString(R.string.redirect_uri_host);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+
+        super.onStart();
+
+        if (preferences.getString("active_access_token", null) != null &&
+                preferences.getString("active_domain", null) != null) {
+            // If we have a token already, we are already logged in
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        Uri uri = getIntent().getData();
+
+        if (uri != null && uri.toString().startsWith(redirectUrl)) {
+            handleAuthorisation(uri);
+        }
     }
 
     @Override
@@ -66,6 +96,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+
+        super.onStop();
+
+        if (domain != null) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("active_domain", domain);
+            editor.putString("active_client_id", clientId);
+            editor.putString("active_client_secret", clientSecret);
+            editor.apply();
+        }
+    }
+
     public void tryLogin(View view) {
         Log.d(TAG, "tryLogin");
 
@@ -76,7 +121,9 @@ public class LoginActivity extends AppCompatActivity {
 
         if (storedClientId != null && storedClientSecret != null) {
             Log.i(TAG, "Found stored client tokens");
-            // TODO: Request an auth token with stored client tokens
+            Log.i(TAG, "clientId:     " + clientId);
+            Log.i(TAG, "clientSecret: " + clientSecret);
+
             login();
         } else {
             Log.i(TAG, "No stored tokens found");
@@ -111,17 +158,21 @@ public class LoginActivity extends AppCompatActivity {
     private void registerApp(final String domain) {
         Log.d(TAG, "registerApp");
 
-        Callback<OAuthTokens> callback = new Callback<OAuthTokens>() {
+        Callback<ClientTokens> callback = new Callback<ClientTokens>() {
             @Override
-            public void onResponse(Call<OAuthTokens> call, Response<OAuthTokens> response) {
+            public void onResponse(Call<ClientTokens> call, Response<ClientTokens> response) {
                 if (!response.isSuccessful()) {
-                    Log.e(TAG, "registerApp Failed: " + response.message());
-                    domainEditText.setError("Failed to register with " + domain + ": " + response.message());
+                    Log.e(TAG, "Failed to register app: " + response.message());
                     return;
                 }
-                OAuthTokens tokens = response.body();
+
+                ClientTokens tokens = response.body();
                 clientId = tokens.clientId;
                 clientSecret = tokens.clientSecret;
+
+                Log.i(TAG, "Successfully registered app");
+                Log.i(TAG, "clientId:     " + clientId);
+                Log.i(TAG, "clientSecret: " + clientSecret);
 
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.putString(domain + "_client_id", clientId);
@@ -132,18 +183,13 @@ public class LoginActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onFailure(Call<OAuthTokens> call, Throwable t) {
-                Log.e(TAG, "registerApp Failed");
-                domainEditText.setError("Failed to register with " + domain);
+            public void onFailure(Call<ClientTokens> call, Throwable t) {
+                Log.e(TAG, "Failed to register app");
             }
         };
 
-        MastodonApiClient api = new MastodonApiClient(OkHttpClientHelper.getOkHttpClient(), domain);
-        api.getAppService().registerApp(
-                getString(R.string.app_name),
-                getString(R.string.redirect_uri_scheme) + "://" + getString(R.string.redirect_uri_host) + getString(R.string.redirect_uri_path),
-                APP_SCOPE,
-                getString(R.string.app_website))
+        mastodonApi.getAuthService().registerClient(getString(R.string.app_name), redirectUrl,
+                APP_SCOPE, getString(R.string.app_website))
         .enqueue(callback);
     }
 
@@ -154,12 +200,74 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        // TODO: Login
+        String query = "?client_id=" + clientId + "&redirect_uri=" + redirectUrl +
+                "&response_type=code&scope=" + APP_SCOPE;
+        String url = "https://" + domain + MastodonApiClient.OAUTH_AUTHORIZE_PATH + query;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.e(TAG, "No browser available");
+        }
     }
 
-    @OnTextChanged(value = R.id.edittext_domain, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    /*@OnTextChanged(value = R.id.edittext_domain, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
     void unsetError() {
         domainEditText.setError(null);
+    }*/
+
+    private void handleAuthorisation(Uri uri) {
+        // If they match, then it's a redirect from the authorisation page
+        String error = uri.getQueryParameter("error");
+        String code = uri.getQueryParameter("code");
+
+        if (error != null) {
+            Log.e(TAG, "Login failed: " + error);
+        } else if (code != null) {
+            Log.i(TAG, "Successfully authorised");
+            Log.i(TAG, code);
+
+            domain = preferences.getString("active_domain", null);
+            clientId = preferences.getString("active_client_id", null);
+            clientSecret = preferences.getString("active_client_secret", null);
+
+            // Swap the authorization code for an access token
+            Callback<AccessToken> callback = new Callback<AccessToken>() {
+                @Override
+                public void onResponse(Call<AccessToken> call, Response<AccessToken> response) {
+                    if (!response.isSuccessful()) {
+                        Log.e(TAG, "Failed to retrieve access token: " + response.message());
+                        return;
+                    }
+
+                    AccessToken token = response.body();
+                    String accessToken = token.accessToken;
+
+                    Log.i(TAG, "Successfully retrieved access token");
+                    Log.i(TAG, "accessToken:  " + accessToken);
+
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString(domain + "_access_token", accessToken);
+                    editor.putString("active_access_token", accessToken);
+                    editor.apply();
+
+                    Intent intent = new Intent(LoginActivity.this, TitleActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onFailure(Call<AccessToken> call, Throwable t) {
+                    Log.e(TAG, "Failed to retrieve access token");
+                }
+            };
+
+            mastodonApi.getAuthService().getAccessToken(clientId, clientSecret, redirectUrl,
+                    code, "authorization_code").enqueue(callback);
+        } else {
+            Log.e(TAG, "Unknown error occurred.");
+        }
     }
 
 }
--- app/src/main/java/com/kirakiratter/android/TitleActivity.java
+++ app/src/main/java/com/kirakiratter/android/TitleActivity.java
@@ -5,18 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-/**
- * @author Southrop
- */
-
-public class MainActivity extends AppCompatActivity {
+public class TitleActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_title);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 }
